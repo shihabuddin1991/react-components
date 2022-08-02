b2f7232f8fdfb4c5a8889d99296358aefba71933
shihab uddin <104322311+shihabuddin1991@users.noreply.github.com>
2022-08-02 12:13:04 +0600

    yellow

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,5 +1,5 @@
 const CarStyle={color:"white",
-                background:"black",
+                background:"yellow",
                 textAlign:"center",
                 margin:"18px",
                 padding:"20px",
